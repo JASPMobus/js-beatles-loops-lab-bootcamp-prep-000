9f73d12054297e8f65883e4316985aeffc04b9f3
James Mobus <mobusj@live.com>
2019-07-19 19:58:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function theBeatlesPlay(musicians, instruments) {
   var retArray = [];
   
   for(let i = 0; i<4; i++) {
-    retArray.push(musicians[i] + " plays " + instrument[i])
+    retArray.push(musicians[i] + " plays " + instruments[i])
   }
 }
